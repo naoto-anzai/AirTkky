b5ad008f54c3e5658a4024a53bbc1dfa50becbea
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2024-09-29 20:23:12 +0900

    矢印キーによる移動の改善

---
+++ Assets/SceneAssets/Game/Script/for_test/test_tenkey_adforce.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_tenkey_adforce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody myRigidbody;
+
+    bool isKeypressed;
+
+    public float myForce = 20f;
+    public float maxSpeed = 20f;
+    public float speedDecrease = 0.1f;
+    public float minSpeed = 0.01f;
+
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        isKeypressed = false;
+        // ¶‚ÉˆÚ“®
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            isKeypressed = true;
+            myRigidbody.AddForce(new Vector3(-myForce, 0, 0) * Time.deltaTime);
+            UnityEngine.Debug.Log("Left key was pressed.");
+        }
+        // ‰E‚ÉˆÚ“®
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            isKeypressed = true;
+            myRigidbody.AddForce(new Vector3(myForce, 0, 0) * Time.deltaTime);
+            UnityEngine.Debug.Log("Right key was pressed.");
+        }
+        // ‘O‚ÉˆÚ“®
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            isKeypressed = true;
+            myRigidbody.AddForce(new Vector3(0, 0, myForce) * Time.deltaTime);
+            UnityEngine.Debug.Log("Up key was pressed.");
+        }
+        // Œã‚ë‚ÉˆÚ“®
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            isKeypressed = true;
+            myRigidbody.AddForce(new Vector3(0, 0, -myForce) * Time.deltaTime);
+            UnityEngine.Debug.Log("Down key was pressed.");
+        }
+        // Å‘å‘¬“x‚Ì§ŒÀ
+        if (myRigidbody.velocity.magnitude > maxSpeed)
+        {
+            myRigidbody.velocity = myRigidbody.velocity.normalized * maxSpeed;
+        }
+        if (!isKeypressed)
+        {
+            myRigidbody.velocity = myRigidbody.velocity.normalized * speedDecrease;
+
+            if(myRigidbody.velocity.magnitude < minSpeed)
+            {
+                myRigidbody.velocity = myRigidbody.velocity.normalized * 0f;
+            }
+        }
+    }
+}
--- Assets/SceneAssets/Game/Script/handle_tenkey_controller.cs
+++ Assets/SceneAssets/Game/Script/handle_tenkey_controller.cs
@@ -2,34 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class handle_tenkey_controller: MonoBehaviour
+public class handle_tenkey_controller : MonoBehaviour
 {
-    
-    void Start() 
+
+    const float deltaPosition = 3F;
+    void Start()
     {
 
     }
-        
+
     void Update()
     {
         // ç∂Ç…à⁄ìÆ
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            transform.position = transform.position + new Vector3(-deltaPosition, 0, 0) * Time.deltaTime;
             UnityEngine.Debug.Log("Left key was pressed.");
         }
         // âEÇ…à⁄ìÆ
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            transform.position = transform.position + new Vector3(deltaPosition, 0, 0) * Time.deltaTime;
             UnityEngine.Debug.Log("Right key was pressed.");
         }
         // ëOÇ…à⁄ìÆ
         if (Input.GetKey(KeyCode.UpArrow))
         {
+            transform.position = transform.position + new Vector3(0, 0, deltaPosition) * Time.deltaTime;
             UnityEngine.Debug.Log("Up key was pressed.");
         }
         // å„ÇÎÇ…à⁄ìÆ
         if (Input.GetKey(KeyCode.DownArrow))
         {
+            transform.position = transform.position + new Vector3(0, 0, -deltaPosition) * Time.deltaTime;
             UnityEngine.Debug.Log("Down key was pressed.");
         }
 
